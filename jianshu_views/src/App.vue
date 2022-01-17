7b597ec9059c5bad69ef4ff23f44b0a1a12e2924
liuyang <1773587692@qq.com>
2022-01-17 23:10:48 +0800

    feat(功能): 构建vue-cli&&对axios进行封装

---
+++ jianshu_views/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    Hello Jianshu
+    <el-button type="success">按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    this.$http({
+      path: 'http://localhost:3000/users/find',
+      method: 'get',
+      params: {
+
+      }
+    }).then(res => {
+      console.log(res)
+    })
+  }
+  // mounted() {
+  //   this.$axios.get('http://localhost:3000/users/find').then(res => {
+  //     console.log(res.data)
+  //   }).catch(err => {
+  //     console.error('请求异常',err)
+  //   })
+  // }
+}
+</script>
+
+<style>
+
+</style>
