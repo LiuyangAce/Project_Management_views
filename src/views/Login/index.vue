d0cbdd5a7a301d3c2c3d2666694e55d1dd504815
liuyang <1773587692@qq.com>
2023-07-05 17:44:14 +0800

    feat:图片压缩迁移

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="loginBg" v-title data-title="用户登录">
+    <!-- <input id="fileInput" type="file" @click="handleCompress" />
+    <img id="img" src="" alt="" width="100" height="100"> -->
     <el-tabs v-model="activeName" @tab-click="handleClick" type="border-card">
-      <span class="defaultLoginInfor" @click="openLoginInfoNotify"
-        >默认登录信息</span
-      >
+      <span class="defaultLoginInfor" @click="openLoginInfoNotify">默认登录信息</span>
       <!-- src="https://www.madpecker.com/system/static/main/logo.png" -->
       <img src="@/assets/MadPecker.png" alt="" width="200px" height="50px" />
       <el-tab-pane label="登录" name="login" class="login">
@@ -33,11 +33,7 @@
             </el-form-item>
             <el-form-item label="角色:" prop="type">
               <el-select v-model="formReg.type" placeholder="请选择">
-                <el-option
-                  v-for="item in roleOptions"
-                  :key="item.value"
-                  :label="item.label"
-                  :value="item.value">
+                <el-option v-for="item in roleOptions" :key="item.value" :label="item.label" :value="item.value">
                 </el-option>
               </el-select>
             </el-form-item>
@@ -106,9 +102,74 @@ export default {
     }
   },
   created() {
-    console.log('axios baseURL：',process.env.VUE_APP_SERVER_URL);
+    console.log('axios baseURL：', process.env.VUE_APP_SERVER_URL);
   },
   methods: {
+    handleCompress() {
+      let fileId = document.getElementById('fileInput')
+      let img = document.getElementById('img')
+      fileId.onchange = (e) => {
+        let file = e.target.files[0]
+        this.compressImg(file, 0.2).then(res => {
+          console.log(res)
+          img.src = window.URL.createObjectURL(res.file);
+        })
+      }
+    },
+    /**
+     * 压缩方法 
+     * @param {string} file 文件
+     * @param {Number} quality  0~1之间
+    */
+    compressImg(file, quality) {
+      if (file[0]) {
+        return Promise.all(Array.from(file).map(e => compressImg(e,
+          quality))) // 如果是 file 数组返回 Promise 数组
+      } else {
+        return new Promise((resolve) => {
+          const reader = new FileReader() // 创建 FileReader
+          reader.onload = ({
+            target: {
+              result: src
+            }
+          }) => {
+            const image = new Image() // 创建 img 元素
+            console.log('img:',image);
+            image.onload = async () => {
+              const canvas = document.createElement('canvas') // 创建 canvas 元素
+              canvas.width = image.width
+              canvas.height = image.height
+              // canvas.width = 100
+              // canvas.height = 100
+              console.log(image.width,image.height);
+              canvas.getContext('2d').drawImage(image, 0, 0, image.width, image.height) // 绘制 canvas
+              // HTMLCanvasElement.toDataURL() 方法返回一个包含图片展示的 data URI 。
+              // 可以使用 type 参数其类型，默认为 PNG 格式。图片的分辨率为96dpi。
+              const canvasURL = canvas.toDataURL('image/jpeg', quality)
+              const buffer = atob(canvasURL.split(',')[1])
+              let length = buffer.length
+              const bufferArray = new Uint8Array(new ArrayBuffer(length))
+              while (length--) {
+                bufferArray[length] = buffer.charCodeAt(length)
+              }
+              const miniFile = new File([bufferArray], file.name, {
+                type: 'image/jpeg'
+              })
+              resolve({
+                file: miniFile,
+                origin: file,
+                beforeSrc: src,
+                afterSrc: canvasURL,
+                beforeKB: Number((file.size / 1024).toFixed(2)),
+                afterKB: Number((miniFile.size / 1024).toFixed(2))
+              })
+            }
+            image.src = src
+          }
+          reader.readAsDataURL(file)
+        })
+      }
+    },
     openLoginInfoNotify() {
       this.$notify({
         title: 'Login',
@@ -126,7 +187,7 @@ export default {
         offset: 50,
       })
     },
-    handleClick(tab, event) {},
+    handleClick(tab, event) { },
     loginHandler() {
       const option = {
         ...api.USERS_LOGIN,
@@ -185,6 +246,7 @@ export default {
   top: 30px;
   cursor: pointer;
 }
+
 .loginBg {
   background-image: url('../../assets/loginBg.png');
   /* background-image: url('https://www.madpecker.com/system/static/main/loginBg.png'); */
@@ -193,6 +255,7 @@ export default {
   height: 100%;
   width: 100%;
 }
+
 .login {
   display: flex;
   justify-content: center;
@@ -200,6 +263,7 @@ export default {
   height: 100%;
   width: 100%;
 }
+
 .loginCard,
 .regCard {
   /* width: 420px; */
@@ -208,17 +272,20 @@ export default {
   height: 300px;
   overflow-y: auto;
 }
+
 /deep/ .el-tabs--border-card {
   width: 25%;
   position: absolute;
   top: 50%;
   right: 10%;
-  transform: translate(10%,-50%);
+  transform: translate(10%, -50%);
 }
+
 /deep/ #tab-login {
   font-size: 18px;
   color: var(--color);
 }
+
 /deep/ #tab-reg {
   font-size: 18px;
   color: var(--color);
